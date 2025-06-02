d02d27755916c11da0962a9569064b33a588d25b
xieliangji <450324623@qq.com>
2025-06-02 13:18:04 +0800

    [Update] update test

---
+++ hello/update.go
@@ -0,0 +1,7 @@
+package hello
+
+import "fmt"
+
+func JustUpdate() {
+	fmt.Println("Just for updating test.")
+}
---
+++ hello/update_test.go
@@ -0,0 +1,7 @@
+package hello
+
+import "testing"
+
+func TestJustUpdate(t *testing.T) {
+	JustUpdate()
+}
